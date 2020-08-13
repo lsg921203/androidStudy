a1d07628f57c946aa2215cf56a6d533414acd51c
lsg921203 <oallk@naver.com>
2020-08-13 10:58:25 +0900

    listView practice

---
+++ app/src/main/java/com/example/androidstudy/ListViewActivity.java
@@ -0,0 +1,79 @@
+package com.example.androidstudy;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.androidstudy.model.Member;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+    private ArrayList<Member> datas;
+    private ArrayAdapter<Member> aa;
+    private ListView listView;
+    private EditText name;
+    private EditText tel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        listView = findViewById(R.id.lv1);
+        name = findViewById(R.id.name);
+        tel = findViewById(R.id.tel);
+        datas = new ArrayList<Member>();
+
+        // android.R.layout.simple_list_item_1: 안드로이드에서 제공하는 api
+        aa = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, datas);
+        listView.setAdapter(aa);
+        //이벤트 리스너 : 이벤트가 발생했는가 기다렸다가 발생하면 핸들러 호출
+        //setOnItemClickListener: 리스너를 view에 붙여줌
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Member m = datas.get(i);
+                String s = m.getName() + "/" + m.getTel();
+                Toast.makeText(ListViewActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void onSave(View view){
+        String n = name.getText().toString();
+        String t = tel.getText().toString();
+
+        datas.add(new Member(n,t));
+        name.setText("");
+        tel.setText("");
+        aa.notifyDataSetChanged();//데이터 갱신 -> 뷰 갱신
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,Menu.FIRST,0,"add");
+        menu.add(0,Menu.FIRST+1,0,"delete");
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                Toast.makeText(ListViewActivity.this,"option menu:add",Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(ListViewActivity.this,"option menu:delete",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/androidstudy/model/Member.java
@@ -0,0 +1,35 @@
+package com.example.androidstudy.model;
+
+public class Member {
+    private String name;
+    private String tel;
+
+    public Member() {}
+    public Member(String name, String tel) {
+        this.name = name;
+        this.tel = tel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name = " + name + "\n"+
+                "tel = " + tel ;
+    }
+}
